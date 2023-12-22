eef01bf4f0087b0211fa8301c67571fdd1e18aa1
Freddyz5 <freddyltacuri@gmail.com>
2023-12-21 23:01:51 -0500

    chore: :bricks: TASK-40 Defining folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import SideNav from '../src/menu';
+import { montserrat } from '../src/ui/fonts';
+import '../src/global.css';
+
+export default function RootLayout(
+  { children } : { children: React.ReactNode }
+) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased flex h-screen flax-col md:flex-row md:overflow-hidden`}>
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
