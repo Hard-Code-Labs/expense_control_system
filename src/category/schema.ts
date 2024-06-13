7be0cb3979c91d2c58f5c1d9ce4f1fbe129dd78e
Freddyz5 <freddyltacuri@gmail.com>
2024-06-12 20:59:15 -0500

    fix: :bug: Add regex validation to create categories

--- src/category/schema.ts
+++ src/category/schema.ts
@@ -1,7 +1,12 @@
 import { object, string } from "yup";
 
 export const categoriesSchema = object().shape({
-  cat_name: string().required("Este campo es requerido"),
+  //! Permitir que el nombre sean solo letras mayusculas y minusculas y espacios
+  cat_name: string()
+    .required("Este campo es requerido")
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, {
+      message: "El nombre de la farmacia no es válido",
+    }),
   cat_icon: string().required("Este campo es requerido"),
   cat_type: string().required("Este campo es requerido")
 })
