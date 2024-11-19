d762125be98dfd67079c921f235852bed5d26f65
Freddyz5 <freddyltacuri@gmail.com>
2024-11-18 22:31:36 -0500

    refactor: :recycle: Reorder some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import {Providers} from "./providers";
-import { roboto } from '../src/shared/styles/fonts';
+import { roboto } from '../src/shared/fonts';
 import '../src/global.css';
 
 export default function RootLayout(
