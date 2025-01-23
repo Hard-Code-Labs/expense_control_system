4cea1d38a1d941245c95bbb0934a0122556c2ff2
Freddyz5 <freddyltacuri@gmail.com>
2025-01-22 19:54:22 -0500

    feat: :sparkles: Add middleware validation

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,21 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-
 export function middleware(request: NextRequest) {
-  
-  //! Falta agregar validación para el token
-  const jwt = true;
 
-  if (jwt === undefined){
-    return NextResponse.redirect(new URL('/', request.url))
+  const cookieRefreshToken = request.cookies.get('token')?.value
+
+  if (!cookieRefreshToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
-  
-  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/category/:path*', '/data/:path*', '/preferences/:path*'],
+  matcher: [
+    '/dashboard/:path*',
+    '/category/:path*',
+    '/data/:path*',
+    '/preferences/:path*',
+    '/accessDenied/:path*',
+  ],
 }
