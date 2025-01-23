f9b20917d3920d7d5e1e167be0f6566bb27a9f78
Freddyz5 <freddyltacuri@gmail.com>
2025-01-22 19:53:36 -0500

    feat: :sparkles: Restricted access by roles

--- app/(menu)/category/page.tsx
+++ app/(menu)/category/page.tsx
@@ -1,7 +1,14 @@
+'use client';
 import Category from '@/src/category';
+import { useUserAuthStore } from '@/src/shared/store/userAuthStore';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function Page() {
+  const { userInfo } = useUserAuthStore();
+  const router = useRouter();
+  if (!userInfo.roles!.includes('ROLE_ADMIN')) router.push('/accessDenied');
+  
   return (
     <Category />
   );
