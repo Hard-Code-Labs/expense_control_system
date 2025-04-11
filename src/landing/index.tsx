70ba8e994df6aa93805c6e5b666b5905be2199cd
Freddyz5 <freddyltacuri@gmail.com>
2025-04-11 09:18:09 -0500

    feat: :sparkles: Create LandingPage component to render in root

---
+++ src/landing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Examples from './components/Examples';
+import CallToAction from './components/CallToAction';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+
+const Landing = () => {
+  return (
+    <>
+      <h1>Landing page</h1>
+    </>
+  );
+};
+
+export default Landing;
